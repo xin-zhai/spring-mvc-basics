14733b46b83c7d7a2f7e61bcc3dca8588150aa46
zhai xin <xin.zhai@thoughtworks.com>
2020-06-18 01:47:23 +0800

    add user entity

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/dao/User.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.capacity.gtb.mvc.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String mail;
+}
